35a9e976c464caa8852767378927059269910a03
Rocky <rocky.ca@gmail.com>
2018-01-17 22:54:37 -0500

    Make call to getDiffs per section

--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,7 @@
 from flask import render_template, flash, redirect
 from app import app
+from string import Template
+import requests, urllib2
 
 @app.route("/")
 @app.route("/index")
@@ -8,4 +10,12 @@ def index():
 
 @app.route("/diff/<int:title>/<section>/<int:startYear>/<int:endYear>")
 def diff(title, section, startYear, endYear):
-    return render_template("diff.html", title=title, section=section, startYear=startYear, endYear=endYear)
+    diffs = []
+    for s in section.split('+'):
+        #todo parameterize url for requests, figure out how more complicated nested sections look
+        diffs.append(requests.request(method='get', url='http://127.0.0.1:5001/getDiffs', data=getDiffRequestStruct(title, s, startYear, endYear)))
+    return render_template("diff.html", title=title, section=section, startYear=startYear, endYear=endYear, diffs=diffs)
+
+def getDiffRequestStruct(title, section, startYear, endYear):
+    t = Template('{"title":$title, "section":$section, "before":$before, "after":$after}')
+    return t.substitute(title=title, section=section, before=startYear, after=endYear)
