090698c5057a262d1d27b887c76f4a10185339ef
Alena Farber <alenafarber@gmail.com>
2018-01-25 16:56:47 -0500

    about page created; header styled to shrink on scroll on all pages except main

--- app/routes.py
+++ app/routes.py
@@ -11,6 +11,11 @@ TESTING_ONE_SERVER_ONLY = True
 def index():
     return render_template("index.html")
 
+@app.route("/about")
+@app.route("/about.html")
+def about():
+    return render_template("about.html")
+
 @app.route("/diff/<int:title>/<section>/<int:startYear>/<int:endYear>")
 def diff(title, section, startYear, endYear):
     diffs = []
