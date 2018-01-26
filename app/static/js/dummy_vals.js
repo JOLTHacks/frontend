2043314b577d61d1d451192b5cd3495811d65dfa
Alena Farber <alenafarber@gmail.com>
2018-01-26 15:55:40 -0500

    created dummy values

---
+++ app/static/js/dummy_vals.js
@@ -0,0 +1,46 @@
+// DUMMY VALUES
+
+[
+	{
+		"section": 1,
+		"text": [
+			{
+				"indentLevel": 0,
+				"markUpContent": "This is a set of <span class='delete'>fake</span><span class='add'>dummy</span> values."
+			},
+			{
+				"indentLevel": 1,
+				"markUpContent": "<span class='numbering'>(a)</span> Subsection for <span class='delete'>the current section</span><span class='add'>section 1 of Title whatever</span>."
+			},
+			{
+				"indentLevel": 2,
+				"markUpContent": "<span class='numbering'>(1)</span> This is a subsubsection for fun."
+			},
+			{
+				"indentLevel": 1,
+				"markUpContent": "<span class='numbering'>(b)</span> Subsection AGAIN for <span class='delete'>the current section</span><span class='add'>section 1 of Title whatever</span>."
+			},			
+		]
+	},
+	{
+		"section": 2,
+		"text": [
+			{
+				"indentLevel": 0,
+				"markUpContent": "This is a new set of <span class='delete'>fake</span><span class='add'>dummy</span> values."
+			},
+			{
+				"indentLevel": 1,
+				"markUpContent": "<span class='numbering'>(a)</span> Subsection for <span class='delete'>the current section</span><span class='add'>section 2 of Title whatever</span>."
+			},
+			{
+				"indentLevel": 2,
+				"markUpContent": "<span class='numbering'>(1)</span> This is a subsubsection for even more fun."
+			},
+			{
+				"indentLevel": 1,
+				"markUpContent": "<span class='numbering'>(b)</span> Subsection AGAIN AGAIN for <span class='delete'>the current section</span><span class='add'>section 1 of Title whatever</span>."
+			},			
+		]
+	}
+]
